e024aac023117a7a9f49d89b915647ce86d84fce
itina99 <matteo.tinacci@stud.unifi.it>
2022-09-17 10:03:04 +0200

    Changes by leona

--- Code/WebApp/Server/Php/Actions.php
+++ Code/WebApp/Server/Php/Actions.php
@@ -59,7 +59,9 @@
 
 	}
 
-	function updateData() {
+
+
+function updateData() {
 		global $mysqli;
 		if (isset($_POST['id'])) $id = $_POST['id'];
 		if (isset($_POST['status'])) $status = $_POST['status'];
