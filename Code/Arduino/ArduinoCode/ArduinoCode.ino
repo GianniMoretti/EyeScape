87af9859f1aa36dcf85600aa586937840cbe471d
Gianni Moretti <jinnw.moretti99@gmail.com>
2022-09-26 13:48:25 +0200

    Arduino code

--- Code/Arduino/ArduinoCode/ArduinoCode.ino
+++ Code/Arduino/ArduinoCode/ArduinoCode.ino
@@ -224,3 +224,97 @@ void splitString(String str, char Separator){
     }
   }
 }
+
+
+
+
+/*  CODICI SENSORI
+ #include <OneWire.h>
+#include <DallasTemperature.h>
+
+OneWire oneWire(5);
+DallasTemperature temp(&oneWire);
+
+float tc = 0;
+float tf = 0;
+
+void setup(void) {
+  Serial.begin(9600);
+  temp.begin();
+}
+
+void loop(void) { 
+  temp.requestTemperatures(); 
+  tc = temp.getTempCByIndex(0);
+  tf = temp.toFahrenheit(tc);
+  Serial.print(" C  ");
+  Serial.print(tc);
+  Serial.print(" F  ");
+  Serial.println(tf);
+  delay(1000);
+}
+
+#define POWER_PIN  7
+#define SIGNAL_PIN A5
+
+int value = 0; // variable to store the sensor value
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(POWER_PIN, OUTPUT);   // configure D7 pin as an OUTPUT
+  digitalWrite(POWER_PIN, LOW); // turn the sensor OFF
+}
+
+void loop() {
+
+  //lettura sensore livello dell'acqua
+  digitalWrite(POWER_PIN, HIGH);  // turn the sensor ON
+  delay(10);                      // wait 10 milliseconds
+  value = analogRead(SIGNAL_PIN); // read the analog value from sensor
+  digitalWrite(POWER_PIN, LOW);   // turn the sensor OFF
+
+  Serial.print("Sensor value: ");
+  Serial.println(value);
+
+  delay(1000);
+}
+
+
+#include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  // start working...
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+  
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  
+  // DHT11 sampling rate is 1HZ.
+  delay(1500);
+
+  
+
+ */
